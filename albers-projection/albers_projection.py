6980d1064352becbca776a0feb7d645adb483616
philemonheng <phil.hsy@outlook.com>
2021-06-02 14:10:43 +0800

    add to formula

--- albers-projection/albers_projection.py
+++ albers-projection/albers_projection.py
@@ -1,24 +1,32 @@
 import json
+import math
 
 def albers_formula():
 
   pass
 
-def albers_projection(file_name):
-  file = open(file_name)
+def albers_projection(dir, file_name):
+  file = open(dir + file_name)
   geojson_data = json.load(file)
+
+  # Earth's radius / latitude
+  radius = 57
 
   # bbox = [min_lon, min_lat, max_lon, max_lat]
   bbox = geojson_data['bbox']
 
-  # references
-  ref_lon = (bbox[0] + bbox[2]) / 2
-  ref_lat = (bbox[1] + bbox[3]) / 2
+  # reference longitude and latitude
+  lambda_0 = (bbox[0] + bbox[2]) / 2
+  phi_0 = (bbox[1] + bbox[3]) / 2
 
   # standard parallels
-  lat_parallel_1 = (ref_lat + bbox[3]) / 2
-  lat_parallel_2 = (ref_lat + bbox[1]) / 2
+  phi_1 = (phi_0 + bbox[3]) / 2
+  phi_2 = (phi_0 + bbox[1]) / 2
 
+  n = (1/2) * (math.sin(phi_1) + math.sin(phi_2))
+  # theta = n * ()
+
+  new_geojson_data = geojson_data
   # iterate through GeoJSON coords
   for feature in geojson_data['features']:
     for multipolygon in feature['geometry']['coordinates']:
@@ -28,7 +36,10 @@ def albers_projection(file_name):
           # albers_formula(coords)
           pass
 
+  with open('data_converted/' + file_name.split('.')[0] + '_converted.geojson', 'w') as new_geojson_file:
+    json.dump(new_geojson_data, new_geojson_file)
 
-file_name = './data/angola.geojson'
 
-albers_projection(file_name)
+dir = './data/'
+file_name = 'angola.geojson'
+albers_projection(dir, file_name)
