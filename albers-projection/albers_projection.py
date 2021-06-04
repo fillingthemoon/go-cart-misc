4e9e0f70501b8458a628173b5baae309101f7ff2
philemonheng <phil.hsy@outlook.com>
2021-06-04 21:12:30 +0800

    fix radians bug

--- albers-projection/albers_projection.py
+++ albers-projection/albers_projection.py
@@ -2,16 +2,16 @@ import json
 import math
 
 def albers_formula(bbox, coords):
-  min_lon = bbox[0]
-  min_lat = bbox[1]
-  max_lon = bbox[2]
-  max_lat = bbox[3]
+  min_lon = math.radians(bbox[0])
+  min_lat = math.radians(bbox[1])
+  max_lon = math.radians(bbox[2])
+  max_lat = math.radians(bbox[3])
 
-  lon = coords[0]
-  lat = coords[1]
+  lon = math.radians(coords[0])
+  lat = math.radians(coords[1])  
 
   # What is this value? TBC
-  radius = 200
+  radius = 1
 
   # reference longitude and latitude
   lambda_0 = (min_lon + max_lon) / 2
@@ -22,6 +22,11 @@ def albers_formula(bbox, coords):
   phi_2 = (phi_0 + min_lat) / 2
 
   n = (1/2) * (math.sin(phi_1) + math.sin(phi_2))
+
+  # TODO
+  # if (math.sin(phi_1) + math.sin(phi_2)):
+  #   Deal with this
+
 
   theta = n * (lon - lambda_0)
   c = ((math.cos(phi_1)) ** 2) + (2 * n * math.sin(phi_1))
@@ -50,14 +55,16 @@ def albers_projection(dir, file_name):
       # if len(multipolygon) > 1: 2nd polygon onwards are holes
       for k, polygon in enumerate(multipolygon):
         for l, coords in enumerate(polygon):
-          new_coords = albers_formula(bbox, coords)
+          new_coords = albers_formula(bbox, coords) # coords = [lon, lat]
           new_geojson_data['features'][i]['geometry']['coordinates'][j][k][l] = new_coords
 
-  with open('data_converted/' + file_name.split('.')[0] + '_converted.geojson', 'w') as new_geojson_file:
+  with open('data-converted/' + file_name.split('.')[0] + '_converted.geojson', 'w') as new_geojson_file:
     json.dump(new_geojson_data, new_geojson_file)
 
 
 dir = './data/'
-file_name = 'angola.geojson'
+# file_name = 'angola.geojson'
+# file_name = 'israel.geojson'
+file_name = 'singapore_pa.geojson'
 # file_name = 'world.geojson'
 albers_projection(dir, file_name)
